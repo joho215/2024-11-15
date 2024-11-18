29b3a31ebb1d1d8a8546d0421de92509c8494ed2
joho215 <xu9873641@gmail.com>
2024-11-18 11:10:24 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    users = [
+        {'name':'ケイスケ', 'age':22},
+        {'name':'ダイキ', 'age':25},
+        {'name':'セイジ', 'age':18},
+    ]
+    return render_template(
+            'users.html',
+            users=users)
+
+@app.route('/page2')
+def page2():
+    users = [
+        {'name':'ダイキ', 'age':25},
+    ]
+    return render_template(
+            'user1.html',
+            users=users)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
+
